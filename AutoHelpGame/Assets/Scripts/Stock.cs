b386864c07a9033a5d1e1f5eaf6d81e0f001df8d
ihateEveryone <vlad.zabannyx.2000@mail.ru>
2022-07-20 23:47:02 +0500

    SaveObjects

--- AutoHelpGame/Assets/Scripts/Stock.cs
+++ AutoHelpGame/Assets/Scripts/Stock.cs
@@ -6,23 +6,24 @@ using UnityEngine.SceneManagement;
 public class Stock : MonoBehaviour
 {
    public  GameObject obj;
+  
+
     
     public void PressButton(int scenenumber)
     {
-       
         SceneManager.LoadScene(scenenumber,LoadSceneMode.Single);
-        
     }
     public void ShowPanel()
     {
         obj.SetActive(true);
-       // SceneManager.LoadScene(scenenumber, LoadSceneMode.Single);
-
     }
     public void HidePanel()
     {
         obj.SetActive(false);
-        // SceneManager.LoadScene(scenenumber, LoadSceneMode.Single);
-
+    }
+    public void Buy1Wheel()
+    {
+        WheelSpin.amountWheel++;
+        PlayerPrefs.SetInt("AviableWheel", WheelSpin.amountWheel);
     }
 }
