a667f0ef8923660442b43f701277571fd25691e1
ihateEveryone <vlad.zabannyx.2000@mail.ru>
2022-07-22 13:52:48 +0500

    EndLevelCoins

--- AutoHelpGame/Assets/Scripts/CarDeath.cs
+++ AutoHelpGame/Assets/Scripts/CarDeath.cs
@@ -4,28 +4,31 @@ using UnityEngine;
 
 public class CarDeath : MonoBehaviour
 {
-
+    public int test = 0;
     void OnTriggerEnter2D(Collider2D col)
     {
-        
-        if(LayerMask.LayerToName(col.gameObject.layer) == "Ground")
+
+        if (LayerMask.LayerToName(col.gameObject.layer) == "Ground")
         {
             Debug.Log(col.gameObject.name + " : " + gameObject.name + " : " + Time.time);
             Debug.Log("death");
-            GameObject.FindWithTag("DeathMenu").transform.GetChild (0).gameObject.SetActive(true);
-            Time.timeScale = 0f;
+            GameObject.FindWithTag("DeathMenu").transform.GetChild(0).gameObject.SetActive(true);
+            Time.timeScale = 2f;
+            GameObject.FindGameObjectWithTag("EndLevelCoins").GetComponent<CoinCounter>().GetValueCoins();
+            GameObject.FindGameObjectWithTag("EndLevelCoins").transform.localPosition = new Vector2(320f, 93.2f);
+
         }
-       
+
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
